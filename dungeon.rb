5d7ba832f30423cd5a4e44c95c84d715548efc62
Julian Møller <julian@julianmoeller.dk>
2012-09-13 17:33:29 +0200

    Add neighbors method and remove getter methods in favor of attr_reader

--- dungeon.rb
+++ dungeon.rb
@@ -1,30 +1,101 @@
 class Dungeon
-	def initialize(width, height, floorprob)
+	attr_reader :tiles, :a, :b, :c, :width, :height, :floorprobability
+
+	def initialize(width, height, floorprobability)
 		@width = width
 		@height = height
-		@floorprob = floorprob
+		@floorprobability = floorprobability
 
 		@tiles = Array.new(@width * @height, :wall)
 
 		create!
 	end
 
+
+	def neighbors(index)
+		xs, ys = i2c(index)
+
+		neighbors = []
+
+		(ys-1..ys+1).to_a.each do |y|
+			(xs-1..xs+1).to_a.each do |x|
+				next if x < 0 or x == @width
+				next if y < 0 or y == @height
+				next if x == xs and y == ys
+
+				neighbors << c2i(x, y) if cell(x, y) == :floor
+			end
+		end
+
+		neighbors
+	end
+
+	def to_s
+		padwidth = @width + 2
+		padheight = @height + 2
+
+		maze = ""
+
+		padheight.times do |y|
+			line = ""
+
+			padwidth.times do |x|
+				if (x == 0 and y == 0) or
+					(x == padwidth - 1 and y == 0) or
+					(x == padwidth - 1 and y == padheight - 1) or
+					(x == 0 and y == padheight - 1) then
+					line += "+"
+				elsif (x == 0 or x == padwidth - 1) then
+					line += "|"
+				elsif (y == 0 or y == padheight - 1) then
+					line += "-"
+				else
+					case c2i(x-1, y-1)
+					when @posa
+						line += "A"
+					when @posb
+						line += "B"
+					when @posc
+						line += "C"
+					else
+						line += cell_to_s(x - 1, y - 1)
+					end
+				end
+			end
+
+			maze += line + "\n"
+		end
+
+		"[ #{@width}, #{@height}, #{@floorprobability} ]\n" +
+		"[ A: #{@posa}, B: #{@posb}, C: #{@posc} ]\n" +
+		"#{maze}"
+	end
+
+	private
+
 	def create!
-		prob = @floorprob || 0.5
+		prob = @floorprobability || 0.5
+
+		floortiles = []
 
 		@tiles.length.times do |index|
 			if rand(nil) < prob then
 				@tiles[index] = :floor
+				floortiles << index
 			end
 		end
-	end
 
-	def tiles
-		@tiles
+		@a = floortiles.shift
+		@c = floortiles.pop
+		@b = floortiles.sample
 	end
 
 	def c2i(x, y)
 		y * @width + x
+	end
+
+	def i2c(i)
+		[i % @width, i / @width]
 	end
 
 	def cell(x, y)
@@ -40,29 +111,4 @@ class Dungeon
 		end
 	end
 
-	def to_s
-		padwidth = @width + 2
-		padheight = @height + 2
-
-		padheight.times do |y|
-			str = ""
-
-			padwidth.times do |x|
-				if (x == 0 and y == 0) or
-					(x == padwidth - 1 and y == 0) or
-					(x == padwidth - 1 and y == padheight - 1) or
-					(x == 0 and y == padheight - 1) then
-					str += "+"
-				elsif (x == 0 or x == padwidth - 1) then
-					str += "|"
-				elsif (y == 0 or y == padheight - 1) then
-					str += "-"
-				else
-					str += cell_to_s(x - 1, y - 1)
-				end
-			end
-
-			puts str
-		end
-	end
 end
