b4cbdd1a3cf729f34016bd14763f505f98b7ef9f
Julian Møller <julian@julianmoeller.dk>
2012-09-16 17:43:43 +0200

    Refactor the evolution into a class to prepare for meta-evolution

--- evolution.rb
+++ evolution.rb
@@ -1,125 +1,147 @@
 load "dungeon.rb"
 load "astar.rb"
 
-@width = 10
-@height = 10
-@probability = 0.1
-@mutate_probability = 0.1
-@xswitch = 5
-@yswitch = 5
+class Evolution
+	attr_reader :floor_probability, :mutate_probability, :crossover_block_width, :crossover_block_height,
+		:population_kill_count, :population_mutate_count, :population_crossover_count,
+		:fitness
 
-def gen_population(size)
-	population = Array.new(size)
-	size.times do |i|
-		population[i] = { :dungeon => Dungeon.new(@width, @height, @probability), :fitness => -1, :calculated => false }
-	end
+	def initialize(floor_probability, mutate_probability, crossover_block_width, crossover_block_height,
+		       population_kill_count, population_mutate_count, population_crossover_count)
 
-	population
-end
+		@width = 10
+		@height = 10
 
-def mutate_population(population, count)
-	pop = Marshal.load(Marshal.dump(population.take(count)))
+		@floor_probability = floor_probability
+		@mutate_probability = mutate_probability
+		@crossover_block_width = crossover_block_width
+		@crossover_block_height = crossover_block_height
 
-	pop.map! do |genome|
-		{ :dungeon => genome[:dungeon].mutate(@mutate_probability),
-			:fitness => -1,
-			:calculated => false
-		}
-	end
+		@population_size = 100
+		@iterations = 100
 
-	pop
-end
+		@population_kill_count = population_kill_count
+		@population_mutate_count = population_mutate_count
+		@population_crossover_count = population_crossover_count
 
-def crossover_population(population, parentcount)
-	pop = Marshal.load(Marshal.dump(population.take(parentcount)))
+		@filename  = "floor-#{@floor_probability}_"
+		@filename += "kill-#{@population_kill_count}_"
+		@filename += "crossover-#{@population_crossover_count}-#{@crossover_block_width}-#{@crossover_block_height}_"
+		@filename += "mutate-#{@population_mutate_count}-#{@mutate_probability}"
 
-	children = []
-
-	pop.each_slice(2) do |ps|
-		children << Dungeon.crossover(ps[0], ps[1], @xswitch, @yswitch)
-	end
-
-	children
-end
-
-@population_size = 100
-@pop_worst_count = 50
-
-# if the following numbers combined are not less than or equal @pop_worst_count, the array will be truncated
-# if the numbers combined are less than @pop_worst_count, the remaining places will be filled with new genomes
-@pop_mutate_count = 20
-@pop_crossover_parent_count = 30 # will result in (count / 2) children
-# end numbers
-
-@iterations = 100
-
-@population = gen_population(@population_size)
-
-data = "iteration, maximum fitness\n"
-
-last_iteration = 0
-diff = 100
-
-@iterations.times do |i|
-	# calculate fitness for population
-	@population.map! do |genome|
-		# the unchanged dungeons don't need to be updated
-		genome if genome[:calculated] == true
-
-		d = genome[:dungeon]
-
-		if d.a == nil or d.b == nil or d.b == nil
-			genome[:fitness] = -1
+		if @population_kill_count < (@population_mutate_count + @population_crossover_count) then
+			@fitness = -1
+		elsif @population_kill_count >= @population_size then
+			@fitness = -1
+		elsif (@population_crossover_count * 2) > (@population_size - @population_kill_count)
+			@fitness = -1
 		else
-			ab = AStar.pathlength(d, d.a, d.b)
-			bc = AStar.pathlength(d, d.b, d.c)
-			ca = AStar.pathlength(d, d.c, d.a)
+			@fitness = evolve
+		end
+	end
 
-			if ab < 0 or bc < 0 or ca < 0 then
-				genome[:fitness] = -1
+
+	private
+
+	def evolve
+		@population = gen_population(@population_size)
+
+		data = "iteration, maximum fitness\n"
+		@iterations.times do |i|
+			# calculate fitness for population
+			@population.map! do |genome|
+				# the unchanged dungeons don't need to be updated
+				genome if genome[:calculated] == true
+
+				d = genome[:dungeon]
+
+				if d.a == nil or d.b == nil or d.b == nil
+					genome[:fitness] = -1
+				else
+					ab = AStar.pathlength(d, d.a, d.b)
+					bc = AStar.pathlength(d, d.b, d.c)
+					ca = AStar.pathlength(d, d.c, d.a)
+
+					if ab < 0 or bc < 0 or ca < 0 then
+						genome[:fitness] = -1
+					else
+						genome[:fitness] = ab + bc + ca
+					end
+				end
+
+				genome[:calculated] = true
+
+				genome
+			end
+
+			@population.sort_by! { |g| -g[:fitness] }
+
+			data += "#{i}, #{@population.first[:fitness]}\n"
+
+			# throw away the worst
+			@population.pop(@population_kill_count)
+
+			# crossover. count is multiplied by two to get the number of parents
+			@population += crossover_population(@population, @population_crossover_count * 2)
+
+			# mutate
+			@population += mutate_population(@population, @population_mutate_count)
+
+			# truncate population if there are too many genomes in it
+			if @population.size > @population_size then
+				@population.pop(@population.size - @population_size)
 			else
-				genome[:fitness] = ab + bc + ca
+				# generate some new dungeons to maintain variety
+				@population += gen_population(@population_size - @population.size)
 			end
 		end
+		
+		@population.sort_by! { |g| -g[:fitness] }
+		maze = @population.first
 
-		genome[:calculated] = true
+		File.open("data_#{@filename}.csv", "w") { |f| f.write(data) }
+		File.open("maze_#{@filename}.txt", "w") { |f| f.write(maze) }
 
-		genome
+		maze[:fitness]
 	end
 
-	@population.sort_by! { |g| -g[:fitness] }
+	def gen_population(size)
+		population = Array.new(size)
+		size.times do |i|
+			population[i] = { :dungeon => Dungeon.new(@width, @height, @floor_probability), :fitness => -1, :calculated => false }
+		end
 
-	data += "#{i}, #{@population.first[:fitness]}\n"
-
-	# throw away the worst
-	@population.pop(@pop_worst_count)
-
-	# crossover
-	@population += crossover_population(@population, @pop_crossover_parent_count)
-
-	# mutate
-	@population += mutate_population(@population, @pop_mutate_count)
-
-	# truncate population if there are too many genomes in it
-	if @population.size > @population_size then
-		@population.pop(@population.size - @populationsize)
-	else
-		# generate some new dungeons to maintain variety
-		@population += gen_population(@population_size - @population.size)
+		population
 	end
 
+	def mutate_population(population, count)
+		pop = Marshal.load(Marshal.dump(population.take(count)))
 
-	if i >= last_iteration + diff
-		print "#{i}.."
-		last_iteration = i
+		pop.map! do |genome|
+			{ :dungeon => genome[:dungeon].mutate(@mutate_probability),
+				:fitness => -1,
+				:calculated => false
+			}
+		end
+
+		pop
 	end
+
+	def crossover_population(population, parentcount)
+		pop = Marshal.load(Marshal.dump(population.take(parentcount)))
+
+		children = []
+
+		pop.each_slice(2) do |ps|
+			children << {
+				:dungeon => Dungeon.crossover(ps[0][:dungeon], ps[1][:dungeon], @crossover_block_width, @crossover_block_height),
+				:fitness => -1, :calculated => false
+			}
+		end
+
+		children
+	end
+
 end
 
-puts "#{@iterations}!"
-
-@population.sort_by! { |g| -g[:fitness] }
-maze = @population.first
-
-filename_suffix = "crossover_mutateprob_#{@mutate_probability}_wallprob_#{@probability}"
-File.open("data_#{filename_suffix}.csv", "w") { |f| f.write(data) }
-File.open("maze_#{filename_suffix}.txt", "w") { |f| f.write(maze) }
+Evolution.new(0.7, 0.1, 5, 5, 50, 20, 15)
