14bf33d3d9ee84b14df890ed06adabb5866a203c
Julian Møller <julian@julianmoeller.dk>
2012-09-16 16:14:35 +0200

    Add some helper methods to Array

---
+++ array_sampleif.rb
@@ -0,0 +1,17 @@
+class Array
+	def sampleif
+		begin
+			a = self.sample
+		end until yield(a)
+
+		a
+	end
+
+	def sampleindexif
+		begin
+			index = rand(self.length)
+		end until yield(self[index], index)
+		
+		index
+	end
+end
